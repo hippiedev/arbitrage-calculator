f36a14d4accb660aa8d9405fde886f86143bdd13
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2022-05-13 23:01:06 +0100

    something

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import AddOdds from "./Components/AddOdds/AddOdds";
 import Results from "./Components/Results/Results";
 import "./App.css";
